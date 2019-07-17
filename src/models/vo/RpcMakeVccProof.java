158bba34f16aa57b71cb89620e888582143b189e
huochengyan <929914120@qq.com>
2019-07-17 16:14:21 +0800

    增加生成支票证明，  生成取消支票的证明。获取节点运行信息

---
+++ src/models/vo/RpcMakeVccProof.java
@@ -0,0 +1,81 @@
+package models.vo;
+
+/**
+ * Created by thk on 7/17/19.
+ */
+public class RpcMakeVccProof {
+    private  String   chainId;
+    private  String   from;
+    private  String   to;
+    private  String   fromChainId;
+    private  String   toChainId;
+    private  String   value;
+    private  String   expireheight;
+    private  String   nonce;
+
+    public String getChainId() {
+        return chainId;
+    }
+
+    public void setChainId(String chainId) {
+        this.chainId = chainId;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFromChainId() {
+        return fromChainId;
+    }
+
+    public void setFromChainId(String fromChainId) {
+        this.fromChainId = fromChainId;
+    }
+
+    public String getToChainId() {
+        return toChainId;
+    }
+
+    public void setToChainId(String toChainId) {
+        this.toChainId = toChainId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getExpireheight() {
+        return expireheight;
+    }
+
+    public void setExpireheight(String expireheight) {
+        this.expireheight = expireheight;
+    }
+
+    public String getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
+
+
+}
